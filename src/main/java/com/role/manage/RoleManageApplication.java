85b07e8e910022c022d79d11d7a9dcb0fb671ef3
MCC <machangchang@yunteck.cn>
2019-12-13 15:58:52 +0800

    feat:角色权限系统创建

---
+++ src/main/java/com/role/manage/RoleManageApplication.java
@@ -0,0 +1,13 @@
+package com.role.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/role/manage/RoleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.role.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
